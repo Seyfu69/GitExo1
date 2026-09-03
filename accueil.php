014f92961d0a03e93ad1737fc559401032bacbb9
Adem Zentici <adem.zentici@lmdsio.fr>
2026-09-03 14:20:57 +0200

    Mon projet version 2

--- accueil.php
+++ accueil.php
@@ -1,4 +1,22 @@
-<?php $prenom = $_POST['prenom'];
+
+<?php
+
+$prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 echo "Bonjour $prenom $nom";
+
+
+$login = $_POST['login'];
+$mdp = $_POST['mdp'];
+
+$host = $_SERVER['HTTP_HOST'];
+// on récupère le nom de l'hôte
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\'); // on récupère le début de l'URL
+
+if ($login == "admin" && $mdp == "azerty") {
+    header("Location: http://$host$uri/profil.html"); // vers profil.html
+
+} else {
+    header("Location: http://$host$uri/index.html"); // vers index.html
+    }
 ?>
